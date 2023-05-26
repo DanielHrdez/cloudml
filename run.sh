db9c0875c53c9eff0ac53094122caca56aa8107a
Daniel <daniel@danielhrdez.dev>
2023-05-26 11:00:43 +0100

    some changes

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-(cd cloudml-frontend; npm run build:reload) & (cd cloudml-backend; DEBUG=true uvicorn app:app --reload)
+(cd cloudml-frontend; npm run build:reload) & (cd cloudml-backend; DEBUG=true uvicorn app:app --host 0.0.0.0 --reload)
