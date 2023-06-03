b798407a6b37e2c5ce4aa899670f20acba01810e
Daniel Hernández de León <alu0101331720@ull.edu.es>
2023-06-03 20:39:31 +0100

    a

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 $env:DEBUG="true"
-(Set-Location cloudml-frontend && npm run build:reload) &
-(Set-Location cloudml-backend && uvicorn app:app --reload)
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "Set-Location cloudml-frontend; npm run build:reload"
+Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "Set-Location cloudml-backend; uvicorn app:app --reload"
