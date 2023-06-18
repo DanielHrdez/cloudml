fbb36f9dde4fd4f075e14799b462ed623350dc4a
Daniel Hernández de León <alu0101331720@ull.edu.es>
2023-06-18 17:42:09 +0100

    little issue in dist

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 (cd cloudml-frontend; npm run build:reload) &
-(cd cloudml-backend; DEBUG=true uvicorn app:app --host 0.0.0.0 --reload)
+(cd cloudml-backend; uvicorn app:app --host 0.0.0.0 --reload)
